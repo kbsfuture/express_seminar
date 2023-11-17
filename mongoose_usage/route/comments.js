746ea754f62181c30224da4ec2830d8be264c84e
kbsfuture <konoisland@naver.com>
2023-11-17 12:02:58 +0900

    description on the code added

--- mongoose_usage/route/comments.js
+++ mongoose_usage/route/comments.js
@@ -11,6 +11,9 @@ router.post("/", async (req, res, next) => {
     });
     console.log(comment);
     const result = await Comment.populate(comment, { path: "commenter" });
+    // populate를 통해 댓글의 작성자 정보를 가져와 댓글 객체에 추가하는 작업을 수행
+    // populate 메서드를 사용하여 문서를 가져올 때, 해당 문서와 연결된 다른 컬렉션(모델)의 정보를 가져오려면
+    // 스키마에서 ref 필드가 지정되어 있어야 합니다
     res.status(201).json(result);
   } catch (err) {
     console.error(err);
--- mongoose_usage/schemas/comments.js
+++ mongoose_usage/schemas/comments.js
@@ -9,7 +9,8 @@ const commentSchema = new Schema({
     type: ObjectId,
     required: true,
     ref: "User",
-    //mongoose는 모델을 찾을 때 해당 모델의 이름을 기반으로 모델을 찾습니다. 모델을 정의한 스키마와 관련된 파일에서 모델 이름을 지정한 후, 다른 파일에서 모델 이름을 동일하게 사용하면 Mongoose는 그 모델을 찾아 사용합니다.
+    //mongoose는 모델을 찾을 때 해당 모델의 이름을 기반으로 모델을 찾습니다.
+    //모델을 정의한 스키마와 관련된 파일에서 모델 이름을 지정한 후, 다른 파일에서 모델 이름을 동일하게 사용하면 Mongoose는 그 모델을 찾아 사용합니다.
   },
   comment: {
     type: String,
@@ -22,3 +23,4 @@ const commentSchema = new Schema({
 });
 
 module.exports = mongoose.model("Comment", commentSchema, "commento");
+//3번째 인수 명대로 몽고db collection 이름이 형성됩니다
